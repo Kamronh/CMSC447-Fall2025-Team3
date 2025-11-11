43ddf068d602b6b785c7584a6e5e24c2db667171
upsetstomach <mi64214.@umbc.edu>
2025-11-11 14:29:35 -0500

    Deployed 55177fb with MkDocs version: 1.6.1

--- js/generator.js
+++ js/generator.js
@@ -2,9 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Get references to the button, output textarea, and all checkboxes
     const generateBtn = document.getElementById('generate-btn');
-
-    //Get reference to light/Dark Mode Button
-    const toggleBtn = document.getElementById('toggle-light-button');
     
     // --- FIX 1 & 2 ---
     // 1. Use the correct ID: 'output-content'
@@ -38,10 +35,4 @@ document.addEventListener('DOMContentLoaded', () => {
             outputDiv.innerHTML = finalText;
         }
     });
-    /* Code for light dark mode toggle button
-    function lightDark(){
-        var element = document.body;
-        element.classList.toggle("toggle-light-button")
-    }
-    */
 });
